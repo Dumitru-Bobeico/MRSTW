1843bf14e1f389efe76c2a8c524e477177e05203
Dumitru Bobeico <dima.bobeico.2003@gmail.com>
2024-03-12 09:43:20 +0200

    Finished bundles

--- ChatFlow.Web/App_Start/BundleConfig.cs
+++ ChatFlow.Web/App_Start/BundleConfig.cs
@@ -8,10 +8,17 @@ namespace ChatFlow.Web.App_Start {
      public class BundleConfig {
           public static void RegisterBundles(BundleCollection bundles) {
                bundles.Add(new StyleBundle("~/assets/vendors")
-                    .Include("~/assets/vendors/mdi/css/materialdesignicons.min.css")
-                    .Include("~/assets/vendors/css/vendor.bundle.base.css"));
-               bundles.Add(new StyleBundle("~/assets/css")
-                    .Include("~/assets/css/style.css"));
+                    .Include("~/assets/vendors/mdi/css/materialdesignicons.min.css",
+                    "~/assets/vendors/css/vendor.bundle.base.css",
+                    "~/assets/css/style.css"));
+
+               bundles.Add(new ScriptBundle("~/assets/js")
+                    .Include("~/assets/vendors/js/vendor.bundle.base.js",
+                    "~/assets/js/off-canvas.js",
+                    "~/assets/js/hoverable-collapse.js",
+                    "~/assets/js/misc.js",
+                    "~/assets/js/settings.js",
+                    "~/assets/js/todolist.js"));
           }
      }
 }
